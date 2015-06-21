631bc5a8d930cd3a8fc979e184c7660141e1d895
Sofia Melnikova <s.melnikova@appheads.ru>
2015-06-21 11:22:10 +0400

    bugs фона+нажатие на меню

--- js/script.js
+++ js/script.js
@@ -1,15 +1,21 @@
 /**
  * Created by Sofia on 18.06.2015.
  */
+
 $(function() {
     $('a[href^="#"]').click(function (e) {
         var target = $(this).attr('href');
         //Сохраняем значение атрибута href в переменной:
         if (target !== "#") {
             $('html, body').animate(
-                {scrollTop: $(target).offset().top}, 2000, 'easeInOutExpo'
+                {scrollTop: $(target).offset().top}, 2800, 'easeInOutExpo'
             );
             return false;
         }
+    });
+
+    $(window).bind('scroll', function(e){
+        var scrollTop = $(window).scrollTop();
+        $('.back').css('top', (scrollTop * 0.3) + 'px');
     });
 });
